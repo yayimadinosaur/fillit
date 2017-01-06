17f01710139bdf24d1167c1df6bcda5a7a5cdba7
warren fung <wfung@e1z2r6p1.42.us.org>
2017-01-05 20:55:57 -0800

    added some ideas; 2d_array uses 2day array or linked lists or both; map_size calculates result of 2d_array

---
+++ ft_2d_array.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_2d_array.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/05 20:50:06 by wfung             #+#    #+#             */
+/*   Updated: 2017/01/05 20:52:59 by wfung            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+char	*ft_2d_array(char *str, int i)
+{
+
+}
+
+
+//		decide on whether to use 2d array || linked lists w/ 2 data || both
+//
+//		figure out smallest square possible for i (# of shapes)
+//
+//		make function 2d_array build the map (fitting pieces together or attempt to?
+//
+//		name the different solutions out by letter; preferably 'a' - whatever
+//					because letter per shape is used by 'A' - 'Z'
---
+++ ft_map_size.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_map_size.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/05 20:53:22 by wfung             #+#    #+#             */
+/*   Updated: 2017/01/05 20:54:40 by wfung            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+char	*ft_map_size(char *str, int i)		// not sure if need input shape count
+{
+
+}
+
+//		maybe make a function that picks the smallest possible square and 
+//		then move onto printing it?
